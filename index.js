2300ff96e9af440e4b015a696d9a0795a8c2c713
Nahuel Cely <49888743+Nexzard@users.noreply.github.com>
2019-09-30 22:30:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require ('express');
 const mongoose = require ('mongoose');
 const port = process.env.PORT||3000;
+mongoose.connect('mongodb://localhost/trivia')
+const QuestionSchema = new mongoose.Schema({
+  quiestion:{
+    type:String,
+    required;true,
+  }
+});
+const Question = mongoose.model('Question',QuestionSchema);
 const app = express();
 app.get('/', (req,res) =>{
 res.send('hola');
